4fb14e2986da5991fa4e9b530c245d0e3e6ead13
saurabhdabas <developerdabas@gmail.com>
2022-02-15 21:27:31 +0000

    A questionnaire on node.js

--- survey.js
+++ survey.js
@@ -34,25 +34,5 @@ rl.question("What's your name? Nicknames are also acceptable :", (answer) => {
 
 
 
-// const questions = ["What's your name? Nicknames are also acceptable :","What's an activity you like doing?","What do you listen to while doing that?","Which meal is your favourite (eg: dinner, brunch, etc.)","What's your favourite thing to eat for that meal?","Which sport is your absolute favourite?","What is your superpower? In a few words, tell us what you are amazing at!"]
-
-// const askAQuestion = function(){
-//   for(let question of questions){
-//     console.log(question);
-//     rl.question(question, (answer) => {
-//       console.log(`Thank you for your valuable feedback: ${answer}`);
-//     });
-//     rl.close();
-//   }
-// }
-// askAQuestion();
-// function repeatNTimes(action,num){
-//   for(let i = 0 ; i < num ; i ++){
-//     console.log(i);
-//     action();
-//   }
-// }
-
-// repeatNTimes(askAQuestion,questions.length);
 
 
